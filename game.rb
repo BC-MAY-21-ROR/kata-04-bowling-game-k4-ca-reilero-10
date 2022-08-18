9c63a0a4ff8b15ca9f7a7b47546ef1b28b7b6891
Davinson Castañeda <davinsonbmx@gmail.com>
2022-08-18 17:33:32 -0500

    Modify method rules score and strike_bonus
    Co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    Co-authored-by: iMartinDav <aldntmi@gmail.com>
    Co-authored-by: fpreciado3 <fpreciado3@ucol.mx>

--- game.rb
+++ game.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Game # :nodoc:
-  attr_accessor :score, :frame, :pins, :tirada1, :tirada2, :points
+  attr_accessor :score, :frame, :pins, :tirada1, :tirada2, :points, :turn
 
   def initialize(tirada1, tirada2)
-    @rolls = []
+    # @rolls = []
     @score = 0
     @frame = 0
     @pins = 10
@@ -19,52 +19,64 @@ class Game # :nodoc:
 
   def chart
     @points =  Array.new(9) { Array.new(2) }
-    (0..8 ).each do | i | # Turno
-      (0..1).each do | j |# Intentos
-        @points[i][j] = rand(0..10)
-      end
-    end
-    @points.push(Array.new(3){0})
-    print @points
-    @points
+    # (0..8 ).each do | i | # Turno
+    #   (0..1).each do | j |# Intentos
+    #     @points[i][j] = rand(0..10)
+    #   end
+    # end
+    # @points.push(Array.new(3){0})
+    # print @points
+    # @points = [[1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8, 6]]
+    @points = [[10, 0], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8, 6]]
+              
+    return @points
   end
 
   def pointer
     p 'Puntaje'
     result = Array.new(8)
     (0..8 ).each do | k | # Resultado
+
       result[k] = @points[k][0] + @points[k][1]
+
     end
     print result
     result
   end
   
-  def rules_score(_frame)
-    if strike?(@frame)
-      @score += 10 + strike_bonus(@frame)
-      @frame += 1
-    elsif spare?(@frame)
-      @score += 10 + spare_bonus(@frame)
-      @frame += 2
-    else
-      @score += sum_of_balls_in_frame(@frame)
-      @frame += 2
-    end
-    
-  end
-
+  #REcorrer los puntos y envia frame e intento
   def run_score(points)
     @score = 0
-    (0..8).each do | r | 
-      (0..1).each do | q | 
-      #  rules_score(@points[r])
-      print points[r]
+    p 'run'
+    (0..8).each do | r |
+      (0..1).each do | q |
+        rules_score(r,q)
       end
     end
   end
+  
+  def rules_score(i,j)
 
-  def strike?(_frame)
-    @rolls[@frame] == 10
+    turn =  @points[i][j]
+    next_frame = @points[i+1][j].to_i + @points[i+1][j+1].to_i
+
+    print turn
+    puts ''
+     if strike?(turn)
+       @score += 10 + strike_bonus(i+1)
+
+     end
+    # elsif spare?(turn)
+    #   @score += 10 + spare_bonus(turn)
+    #   turn += 2
+    # else
+    #   @score += sum_of_balls_in_frame(turn)
+    #   turn += 2
+    # end
+    
+  end
+  def strike?(turn)
+    turn == 10
   end
 
   def sum_of_balls_in_frame(_frame)
@@ -75,8 +87,10 @@ class Game # :nodoc:
     @rolls[@frame + 2]
   end
 
-  def strike_bonus(_frame)
-    @rolls[@frame + 1] + @rolls[@frame + 2]
+  def strike_bonus(frame)
+    @points[frame][0].to_i + @points[frame][1].to_i
+    
+    # @rolls[@frame + 1] + @rolls[@frame + 2]
   end
 
   def spare?(_frame)
